bc6584cb815675b0a166533fa8997d4aca86742a
bsergiov <81264661+bsergiov@users.noreply.github.com>
2021-08-19 13:46:12 +0200

    init commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>kVtseuG</title>
+</head>
+<body>
+<div class="container">
+    <div class="card text-center mt-5">
+        <div class="card-header">
+            WARNING
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Goo to google</h5>
+            <p class="card-text">You have nothing to do here</p>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
